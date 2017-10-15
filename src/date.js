c2c58803c8278c4e83eb85ec1c5a25c8ecd96634
聂威 <502533466@qq.com>
2017-10-15 18:06:07 +0800

    日期解析

--- src/date.js
+++ src/date.js
@@ -67,5 +67,219 @@ const formatFlags = {
     },
     dd: function(date) {
         return pad(date.getDate());
+    },
+    // 星期几： 短英文表示
+    ddd: function(date, i18n) {
+        return i18n.shortDayNames[date.getDay()];
+    },
+    dddd: function(date, i18n) {
+        return i18n.dayNames[date.getDay()];
+    },
+    M: function(date) {
+        return date.getMonth() + 1;
+    },
+    MM: function(date) {
+        return pad(date.getMonth() + 1);
+    },
+    MMM: function(date, i18n) {
+        return i18n.shortMonthNames[date.getMonth()];
+    },
+    MMMM: function(date, i18n) {
+        return i18n.monthNames[date.getMonth()];
+    },
+    yy: function(date) {
+        return String(date.getFullYear()).substr(2);
+    },
+    yyyy: function(date) {
+        return date.getFullYear();
+    },
+    h: function(date) {
+        return date.getHours() % 12 || 12;
+    },
+    hh: function(date) {
+        return pad(date.getHours() % 12 || 12);
+    },
+    H: function(date) {
+        return date.getHours();
+    },
+    HH: function(date) {
+        return pad(date.getHours());
+    },
+    m: function(date) {
+        return date.getMinutes();
+    },
+    mm: function(date) {
+        return pad(date.getMinutes());
+    },
+    s: function(date) {
+        return date.getSeconds();
+    },
+    ss: function(date) {
+        return pad(date.getSeconds());
+    },
+    S: function(date) {
+        return Math.round(date.getMilliseconds() / 100);
+    },
+    SS: function(date) {
+        return pad(Math.round(date.getMilliseconds() / 10));
+    },
+    SSS: function(date) {
+        return pad(date.getMilliseconds(), 3);
+    },
+    a: function(date, i18n) {
+        return date.getHours() < 12 ? i18n.amPm[0] : i18n.amPm[1];
+    },
+    A: function(date, i18n) {
+        return date.getHours() < 12 ? i18n.amPm[0].toUpperCase() : i18n.amPm[1].toUpperCase();
+    },
+    ZZ: function(date) {
+        const o = date.getTimezoneOffset();
+        return (o > 0 ? '-' : '+') + pad(Math.floor(Math.abs(o) / 60) * 100 + Math.abs(o) % 60, 4);
     }
+};
+
+const parseFlags = {
+    d: [twoDigits, function(d, v) {
+        d.day = v;
+    }],
+    M: [twoDigits, function(d, v) {
+        d.month = v - 1;
+    }],
+    yy: [twoDigits, function(d, v) {
+        const da = new Date(),
+            cent = +('' + da.getFullYear()).substr(0, 2);
+        d.year = '' + (v > 68 ? cent - 1 : cent) + v;
+    }],
+    h: [twoDigits, function(d, v) {
+        d.hour = v;
+    }],
+    m: [twoDigits, function(d, v) {
+        d.minute = v
+    }],
+    s: [twoDigits, function(d, v) {
+        d.second = v;
+    }],
+    yyyy: [fourDigits, function(d, v) {
+        d.year = v;
+    }],
+    S: [/\d/, function(d, v) {
+        d.millisecond = v * 100;
+    }],
+    SS: [/\d{2}/, function(d, v) {
+        d.millisecond = v * 10;
+    }],
+    SSS: [threeDigits, function(d, v) {
+        d.millisecond = v;
+    }],
+    D: [twoDigits, noop],
+    ddd: [word, noop],
+    MMM: [word, monthUpdate("shortMonthNames")],
+    MMMM: [word, monthUpdate("monthNames")],
+    a: [word, function(d, v, i18n) {
+        const val = v.toLowerCase();
+        if (val === i18n.amPm[0]) {
+            d.isPm = false;
+        } else if (val === i18n.amPm[1]) {
+            d.isPm = true;
+        }
+    }],
+    ZZ: [/[\+\-]\d\d:?\d\d/, function(d, v) {
+        const parts = (v + '').match(/([\+\-]\d\d)/gi),
+            minutes;
+        if (parts) {
+            minutes = +(parts[1] * 60) + parseInt(parts[2], 10);
+            d.timezoneOffset = parts[0] === '+' ? minutes : -minutes;
+        }
+    }]
+};
+
+parseFlags.DD = parseFlags.DD;
+parseFlags.dddd = parseFlags.ddd;
+parseFlags.Do = parseFlags.dd = parseFlags.d;
+parseFlags.mm = parseFlags.m;
+parseFlags.hh = parseFlags.H = parseFlags.HH = parseFlags.h;
+parseFlags.MM = parseFlags.M;
+parseFlags.ss = parseFlags.s;
+parseFlags.A = parseFlags.a;
+
+options.masks = {
+    'default': 'yyyy-MM-dd HH:mm:ss',
+    hortDate: 'M/D/yy',
+    mediumDate: 'MMM d, yyyy',
+    longDate: 'MMMM d, yyyy',
+    fullDate: 'dddd, MMMM d, yyyy',
+    shortTime: 'HH:mm',
+    mediumTime: 'HH:mm:ss',
+    longTime: 'HH:mm:ss.SSS'
+}
+
+export function format(dateObj, mask = options.masks['default'], i18n = options.i18n) {
+    if (typeof dateObj === 'number') {
+        dateObj = new Date(dateObj);
+    }
+
+    if (Object.prototype.toString.call(dateObj) !== '[object Date]' || Number.isNaN(dateObj.getTime())) {
+        throw new Error("Invalid Date in format");
+    }
+
+    return mask.replace(token, function($0) {
+        return $0 in formatFlags ? formatFlags[$0](dateObj, i18n) : $0.slice(1, $0.length - 1);
+    })
+}
+
+export function parse(dateStr, format, i18n = options.i18n) {
+    if (typeof format !== 'string') {
+        throw new Error('Invalid format in parse');
+    }
+
+    const _format = options.masks[format] || format;
+
+    let isValid = true,
+        dateInfo = {};
+    _format.replace(token, function($0) {
+        if (parseFlags[$0]) {
+            const info = parseFlags[$0],
+                index = dateStr.search(info[0]);
+            if (!~index) {
+                isValid = false;
+            } else {
+                dateStr.replace(info[0], function(result) {
+                    info[1](dateInfo, result, i18n);
+                    dateStr = dateStr.substr(index + result.length);
+                    return result;
+                })
+            }
+        }
+
+        return parseFloat[$0] ? '' : $0.slice(1, $0.length - 1);
+    })
+
+    if (!isValid) {
+        return false;
+    }
+
+    const today = new Date();
+    if (dateInfo.timezoneOffset != null) {
+        dateInfo.isPm = +(dateInfo.minute || 0) - +dateInfo.timezoneOffset;
+        date = new Date(Date.UTC(
+            dateInfo.year || today.getFullYear(),
+            dateInfo.month || 0,
+            dateInfo.day || 1,
+            dateInfo.hour || 0,
+            dateInfo.minute || 0,
+            dateInfo.second || 0,
+            dateInfo.millisecond || 0
+        ))
+    } else {
+        date = new Date(
+            dateInfo.year || today.getFullYear(),
+            dateInfo.month || 0,
+            dateInfo.day || 1,
+            dateInfo.hour || 0,
+            dateInfo.minute || 0,
+            dateInfo.second || 0,
+            dateInfo.millisecond || 0
+        )
+    }
+    return date;
 }
